8191951a55f3fd4a79c9009a96bdeeced67bc49c
Mir Billadin <mir.billadin4@gmail.com>
2023-08-04 23:53:41 +0600

    Module 21

---
+++ ES6-practice/compareObjectProperty.js
@@ -0,0 +1,13 @@
+// Write a JavaScript program to compare two objects to determine 
+// if the first contains equivalent property values to the second one.
+
+const first = {
+    age : 21,
+    hair : 'black',
+    breard : true,
+}
+const second = {
+    age : 21,
+    hair : 'black',
+    breard : true,
+}
---
+++ Module-21/stringExercise.js
@@ -0,0 +1,24 @@
+// const text = 'I am a web developer. I am a software QA Engineer'
+
+// // console.log(text.split('', 5));
+
+// "Blue Whale".indexOf("Blue"); // returns  0
+// "Blue Whale".indexOf("Blute"); // returns -1
+// "Blue Whale".indexOf("Whale", 0); // returns  5
+// "Blue Whale".indexOf("Whale", 5); // returns  5
+// "Blue Whale".indexOf("Whale", 7); // returns -1
+// "Blue Whale".indexOf(""); // returns  0
+// "Blue Whale".indexOf("", 9); // returns  9
+// "Blue Whale".indexOf("", 10); // returns 10
+// console.log("Blue Whale".indexOf("Whale", 11)); // returns 10
+
+const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+
+console.log(p.replace('dog', 'monkey'));
+console.log(p);
+// Expected output: "The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?"
+
+// const regex = /Dog/i;
+// console.log(p.replace(regex, 'ferret'));
+// Expected output: "The quick brown fox jumps over the lazy ferret. If the dog reacted, was it really lazy?"
+
